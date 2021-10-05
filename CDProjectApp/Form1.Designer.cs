c678bf226c28cd66d5be7319a821b7ec9b3d4102
GuitaristJake3 <47261035+GuitaristJake3@users.noreply.github.com>
2021-10-05 16:27:30 +0100

    Added add data button, a CD data class and a list of CDDataclass objects to add CDs too List of CDs currently have nowhere to go Made everything invisiable at start

--- CDProjectApp/Form1.Designer.cs
+++ CDProjectApp/Form1.Designer.cs
@@ -40,6 +40,7 @@ namespace CDProjectApp
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.genreLabel = new System.Windows.Forms.Label();
             this.genreComboBox = new System.Windows.Forms.ComboBox();
+            this.addDataToListButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,7 +53,7 @@ namespace CDProjectApp
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
-            this.menuStrip1.Size = new System.Drawing.Size(533, 36);
+            this.menuStrip1.Size = new System.Drawing.Size(533, 31);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -73,43 +74,43 @@ namespace CDProjectApp
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.newToolStripMenuItem.Text = "New";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // shareToolStripMenuItem
             // 
             this.shareToolStripMenuItem.Name = "shareToolStripMenuItem";
-            this.shareToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.shareToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.shareToolStripMenuItem.Text = "Share";
             // 
             // deleteToolStripMenuItem
             // 
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.deleteToolStripMenuItem.Text = "Delete";
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.closeToolStripMenuItem.Text = "Close";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 34);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // genreLabel
@@ -121,6 +122,7 @@ namespace CDProjectApp
             this.genreLabel.Size = new System.Drawing.Size(39, 13);
             this.genreLabel.TabIndex = 1;
             this.genreLabel.Text = "Genre:";
+            this.genreLabel.Visible = false;
             // 
             // genreComboBox
             // 
@@ -131,24 +133,37 @@ namespace CDProjectApp
             "Jazz",
             "Classical",
             "Rap"});
-            this.genreComboBox.SelectedIndex = 0;
             this.genreComboBox.Location = new System.Drawing.Point(162, 66);
-            this.genreComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.genreComboBox.Margin = new System.Windows.Forms.Padding(2);
             this.genreComboBox.Name = "genreComboBox";
             this.genreComboBox.Size = new System.Drawing.Size(82, 21);
             this.genreComboBox.TabIndex = 2;
+            this.genreComboBox.Visible = false;
             this.genreComboBox.SelectedIndexChanged += new System.EventHandler(this.genreComboBox_SelectedIndexChanged);
+            // 
+            // addDataToListButton
+            // 
+            this.addDataToListButton.Enabled = false;
+            this.addDataToListButton.Location = new System.Drawing.Point(151, 127);
+            this.addDataToListButton.Name = "addDataToListButton";
+            this.addDataToListButton.Size = new System.Drawing.Size(75, 23);
+            this.addDataToListButton.TabIndex = 3;
+            this.addDataToListButton.Text = "Add Data";
+            this.addDataToListButton.UseVisualStyleBackColor = true;
+            this.addDataToListButton.Visible = false;
+            this.addDataToListButton.Click += new System.EventHandler(this.addDataToListButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.addDataToListButton);
             this.Controls.Add(this.genreComboBox);
             this.Controls.Add(this.genreLabel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -171,6 +186,7 @@ namespace CDProjectApp
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.Label genreLabel;
         private System.Windows.Forms.ComboBox genreComboBox;
+        private System.Windows.Forms.Button addDataToListButton;
     }
 }
 
--- CDProjectApp/Form1.cs
+++ CDProjectApp/Form1.cs
@@ -13,19 +13,24 @@ namespace CDProjectApp
     public partial class Form1 : Form
     {
         string currentGenre;
+        CDDataClass currentCD;
+        List<CDDataClass> cdList;   //List of objects of the class CDDataClass is defined as class variable
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
+            cdList = new List<CDDataClass>();   //The list is created on program startup
         }
 
         private void genreComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             currentGenre = genreComboBox.Items[genreComboBox.SelectedIndex].ToString();
+            addDataToListButton.Enabled = true;
+        }
+
+        private void addDataToListButton_Click(object sender, EventArgs e)
+        {
+            currentCD = new CDDataClass(currentGenre);
+            cdList.Add(currentCD);      //Next we would display this list in a list box
         }
     }
 }
