dcef9b447aa8b35844d154b3f4520d446ab6bf87
GuitaristJake3 <47261035+GuitaristJake3@users.noreply.github.com>
2021-11-28 03:24:57 +0000

    List of CD objects now built from loaded CD library file

--- CDProjectApp/Start.cs
+++ CDProjectApp/Start.cs
@@ -13,14 +13,16 @@ namespace CDProjectApp
 {
     public partial class Start : Form
     {
+        List<CD> cdList;        //List of objects of the class CD is declared as class variable
         public Start()
         {
             InitializeComponent();
+            cdList = new List<CD>();        //The list is created on program startup
         }
 
-        private void newCDButton_Click(object sender, EventArgs e)
+        private void newCDButton_Click(object sender, EventArgs e)      //Opens form to add a new CD on click
         {
-            CDLibrary newCD = new CDLibrary();      //Opens form to add a new CD on click
+            CDLibrary newCD = new CDLibrary();
             newCD.ShowDialog();
         }
 
@@ -29,8 +31,13 @@ namespace CDProjectApp
             if (openCDList.ShowDialog() == DialogResult.OK)
             {
                 TextReader tr = new StreamReader(openCDList.FileName);
-                Console.WriteLine(tr.ReadToEnd());
-                
+                string line;
+                while ((line = tr.ReadLine()) != null)
+                {
+                    string[] cdInfo = line.Split(',');
+                    CD currentCD = new CD(cdInfo[0], cdInfo[1], cdInfo[2], Convert.ToInt16(cdInfo[3]), cdInfo[4], cdInfo[5], Convert.ToInt16(cdInfo[6]));
+                    cdList.Add(currentCD);
+                }
             }
         }
 
